7de756bac2c559ea088fd51b98654749d4776252
Saeed Dashti <saeed.da1994@gmail.com>
2025-11-01 23:09:06 +0330

    make shared component/ button generate some components for ticket

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() title!: string;
+  @Input() icon!: string;
+  @Input() type?: string;
+}
